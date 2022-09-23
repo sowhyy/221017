6becd921f36b08a3b78b2f3751414335c523dd1e
soy <sowhyy90@gmail.com>
2022-09-23 13:54:17 +0900

    ver1

---
+++ assets/js/custom.js
@@ -0,0 +1,297 @@
+/*--------------------------------------------------------------
+# Darkmode 
+--------------------------------------------------------------*/ 
+function darkmodeGo() {
+  const darkModeToggle = document.getElementById('dn'); // 체크박스 정의
+  if (!darkModeToggle) {
+    return !1;
+  } // 체크 박스 없을 시 작동 종료
+  const Realbody = document.querySelector('body');
+  darkModeToggle.addEventListener('change', function (event) {
+    //체크박스의 변화 감지 리스너
+    if (!Realbody.classList.contains('darkmode')) {
+    // 바디에 다크모드 클래스가 없으면
+      Realbody.classList.add('darkmode'); // 다크모드 추가
+      localStorage.setItem('whatMode', darkModeToggle.checked); //whatMode라는 이름의 아이템에 체크박스의 체크 여부를 저장하기
+    } else {
+    // 바디에 다크모드 클래스가 있으면
+      Realbody.classList.remove('darkmode'); // 다크모드 클래스를 제거
+      localStorage.setItem('whatMode', darkModeToggle.checked); //whatMode라는 이름의 아이템에 체크박스의 체크 여부를 저장하기
+    }
+  });
+}
+darkmodeGo();
+document.addEventListener('DOMContentLoaded', function () {
+  const Realbody = document.querySelector('body');
+  const whatMode = localStorage.getItem('whatMode'); //whatMode 아이템 값 불러오기
+
+  if (whatMode === 'false') {
+    // 체크 여부가 false라면, 라이트모드입니다.
+    //이 때 false는 문자열 타입이므로 "" 안에 적어야 합니다.
+    return !1; // 라이트모드이므로 아무런 행동을 할 필요가 없습니다.
+  } else {
+    // 다크모드라면
+    const darkModeToggle = document.getElementById('dn'); //체크박스를 획득
+    darkModeToggle.checked = true; // 체크박스에 체크를 해주기
+    Realbody.classList.add('darkmode'); // 다크모드를 body에 걸어주기
+  }
+});
+
+/**
+ * Template Name: Lonely - v4.8.1
+ * Template URL: https://bootstrapmade.com/free-html-bootstrap-template-lonely/
+ * Author: BootstrapMade.com
+ * License: https://bootstrapmade.com/license/
+ */
+(function () {
+  'use strict';
+
+  /**
+   * Easy selector helper function
+   */
+  const select = (el, all = false) => {
+    el = el.trim();
+    if (all) {
+      return [...document.querySelectorAll(el)];
+    } else {
+      return document.querySelector(el);
+    }
+  };
+
+  /**
+   * Easy event listener function
+   */
+  const on = (type, el, listener, all = false) => {
+    let selectEl = select(el, all);
+    if (selectEl) {
+      if (all) {
+        selectEl.forEach((e) => e.addEventListener(type, listener));
+      } else {
+        selectEl.addEventListener(type, listener);
+      }
+    }
+  };
+
+  /**
+   * Easy on scroll event listener
+   */
+  const onscroll = (el, listener) => {
+    el.addEventListener('scroll', listener);
+  };
+
+  /**
+   * Navbar links active state on scroll
+   */
+  let navbarlinks = select('#navbar .scrollto', true);
+  const navbarlinksActive = () => {
+    let position = window.scrollY + 200;
+    navbarlinks.forEach((navbarlink) => {
+      if (!navbarlink.hash) return;
+      let section = select(navbarlink.hash);
+      if (!section) return;
+      if (
+        position >= section.offsetTop &&
+        position <= section.offsetTop + section.offsetHeight
+      ) {
+        navbarlink.classList.add('active');
+      } else {
+        navbarlink.classList.remove('active');
+      }
+    });
+  };
+  window.addEventListener('load', navbarlinksActive);
+  onscroll(document, navbarlinksActive);
+
+  /**
+   * Scrolls to an element with header offset
+   */
+  const scrollto = (el) => {
+    let header = select('#header');
+    let offset = header.offsetHeight;
+
+    if (!header.classList.contains('header-scrolled')) {
+      offset -= 16;
+    }
+
+    let elementPos = select(el).offsetTop;
+    window.scrollTo({
+      top: elementPos - offset,
+      behavior: 'smooth',
+    });
+  };
+
+  /**
+   * Header fixed top on scroll
+   */
+  let selectHeader = select('#header');
+  if (selectHeader) {
+    let headerOffset = selectHeader.offsetTop;
+    let nextElement = selectHeader.nextElementSibling;
+    const headerFixed = () => {
+      if (headerOffset - window.scrollY <= 0) {
+        selectHeader.classList.add('fixed-top');
+        nextElement.classList.add('scrolled-offset');
+      } else {
+        selectHeader.classList.remove('fixed-top');
+        nextElement.classList.remove('scrolled-offset');
+      }
+    };
+    window.addEventListener('load', headerFixed);
+    onscroll(document, headerFixed);
+  }
+
+  /**
+   * Back to top button
+   */
+  let backtotop = select('.back-to-top');
+  if (backtotop) {
+    const toggleBacktotop = () => {
+      if (window.scrollY > 100) {
+        backtotop.classList.add('active');
+      } else {
+        backtotop.classList.remove('active');
+      }
+    };
+    window.addEventListener('load', toggleBacktotop);
+    onscroll(document, toggleBacktotop);
+  }
+
+  /**
+   * Mobile nav toggle
+   */
+  on('click', '.mobile-nav-toggle', function (e) {
+    select('#navbar').classList.toggle('navbar-mobile');
+    this.classList.toggle('bi-list');
+    this.classList.toggle('bi-x');
+  });
+
+  /**
+   * Mobile nav dropdowns activate
+   */
+  on(
+    'click',
+    '.navbar .dropdown > a',
+    function (e) {
+      if (select('#navbar').classList.contains('navbar-mobile')) {
+        e.preventDefault();
+        this.nextElementSibling.classList.toggle('dropdown-active');
+      }
+    },
+    true
+  );
+
+  /**
+   * Scrool with ofset on links with a class name .scrollto
+   */
+  on(
+    'click',
+    '.scrollto',
+    function (e) {
+      if (select(this.hash)) {
+        e.preventDefault();
+
+        let navbar = select('#navbar');
+        if (navbar.classList.contains('navbar-mobile')) {
+          navbar.classList.remove('navbar-mobile');
+          let navbarToggle = select('.mobile-nav-toggle');
+          navbarToggle.classList.toggle('bi-list');
+          navbarToggle.classList.toggle('bi-x');
+        }
+        scrollto(this.hash);
+      }
+    },
+    true
+  );
+
+  /**
+   * Scroll with ofset on page load with hash links in the url
+   */
+  window.addEventListener('load', () => {
+    if (window.location.hash) {
+      if (select(window.location.hash)) {
+        scrollto(window.location.hash);
+      }
+    }
+  });
+
+  /**
+   * Porfolio isotope and filter
+   */
+  window.addEventListener('load', () => {
+    let portfolioContainer = select('.portfolio-container');
+    if (portfolioContainer) {
+      let portfolioIsotope = new Isotope(portfolioContainer, {
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows',
+      });
+
+      let portfolioFilters = select('#portfolio-flters li', true);
+
+      on(
+        'click',
+        '#portfolio-flters li',
+        function (e) {
+          e.preventDefault();
+          portfolioFilters.forEach(function (el) {
+            el.classList.remove('filter-active');
+          });
+          this.classList.add('filter-active');
+
+          portfolioIsotope.arrange({
+            filter: this.getAttribute('data-filter'),
+          });
+        },
+        true
+      );
+    }
+  });
+
+  /**
+   * Initiate portfolio lightbox
+   */
+  const portfolioLightbox = GLightbox({
+    selector: '.portfolio-lightbox',
+  });
+
+  /**
+   * Portfolio details slider
+   */
+  new Swiper('.portfolio-details-slider', {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 1500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    loop: true, // 무한 반복
+  });
+
+  /**
+   * Skills animation
+   */
+  let skilsContent = select('.skills-content');
+  if (skilsContent) {
+    new Waypoint({
+      element: skilsContent,
+      offset: '80%',
+      handler: function (direction) {
+        let progress = select('.progress .progress-bar', true);
+        progress.forEach((el) => {
+          el.style.width = el.getAttribute('aria-valuenow') + '%';
+        });
+      },
+    });
+  }
+  /**
+   * Initiate Pure Counter
+   */
+  new PureCounter();
+})();
